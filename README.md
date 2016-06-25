d94bc1cf69e3fda0b23100c6714fc05f51a77467
nmante <nmante88@gmail.com>
2016-06-25 04:26:58 -0700

    Added package file generator, readme and dummy libs

---
+++ README.md
@@ -0,0 +1,54 @@
+#Package Config file generator
+
+##Overview
+
+This is a simple python (2 or 3) script for generating package config (`pkg-config` or `.pc`) files.  Feel free to adapt it to your needs/fork it. 
+
+What does it do?
+
+- Interactive prompt for package info (name, version, description)
+- Command line mode for those who don't like interactive
+- Automatic linker command generation 
+
+Provide it a directory with libraries (.so, .dylib, .etc) and it will generate linker commands. Try it out in the test directory to see it how it works.
+
+
+##Example use
+
+Use the interactive mode for a text prompt. There are dummy lib files in the `test_libs` directory (not real libraries, just for demonstration). 
+
+	python main.py -i test_libs 
+
+Provide info directly via command line. Here we provide the name, version and prefix of the package (Boost in this case). The `prefix` or `-p` represents where the main package is installed on your computer. Lastly, we write the output to a package config file (**boost.pc**) in the current directory.
+
+	python main.py -n Boost -v 1.60.0 -p /usr/local/Cellar/boost/1.60.0_2 -o boost.pc
+
+Once you have a .pc file, you should place it in the pkg-config path. One standard path is this directory. Check it to see if there are other .pc files there.  
+
+	/usr/local/lib/pkgconfig/
+
+##Usage
+	usage: python main.py [-h] [-i] [-n PKG_NAME] [-v PKG_VERSION] [-p PKG_PREFIX]
+		       [-d PKG_DESCRIPTION] [-o OUTPUT_FILE]
+		       lib_dir
+
+	Generate a package config file automatically-ish
+
+	positional arguments:
+	  lib_dir               The directory containing the library files. This is
+				normally the 'lib' folder for your specific package.
+
+	optional arguments:
+	  -h, --help            show this help message and exit
+	  -i, --interactive     Use this flag to be prompted for package information
+				(name, version, description)
+	  -n PKG_NAME, --pkg_name PKG_NAME
+				The name of the package
+	  -v PKG_VERSION, --pkg_version PKG_VERSION
+				The version number of the package
+	  -p PKG_PREFIX, --pkg_prefix PKG_PREFIX
+				The prefix to be used in the package config file
+	  -d PKG_DESCRIPTION, --pkg_description PKG_DESCRIPTION
+				The description for the package
+	  -o OUTPUT_FILE, --output_file OUTPUT_FILE
+				The path to write your package config file to 
---
+++ main.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import argparse
+import collections
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Generate a package config file")
+    parser.add_argument("-i", "--interactive", action="store_true", help="Use this flag to be prompted for package information (name, version, description)")
+
+    parser.add_argument("-n", "--pkg_name", default="", type=str, help="The name of the package")
+    parser.add_argument("-v", "--pkg_version", default="", type=str, help="The version number of the package")
+    parser.add_argument("-p", "--pkg_prefix", default="", type=str, help="The prefix to be used in the package config file")
+    parser.add_argument("-d", "--pkg_description", default="", type=str, help="The description for the package")
+    parser.add_argument("lib_dir", type=str, help="The directory containing the library files. This is normally the 'lib' folder for your specific package.")
+
+    parser.add_argument("-o", "--output_file", default="", type=str, help="The path to write your package config file to")
+
+    return parser
+
+def generate_package_file(args):
+
+    # Python 2 uses a different version of input (compared to Python 3)
+    # Let's make it so that 'input' works on both python3 and python2
+    try:
+        input = raw_input
+    except NameError:
+        import builtins;
+        input = builtins.input;
+        pass
+
+    # This block let's us take input from the user and still 
+    # pipe the results to an output file
+    # i.e. 'python main.py -i . > example.pc' will show the prompt on 
+    # screen, and write the output to 'example.pc'
+    old_input = input
+    def input(*args):
+        old_stdout = sys.stdout
+        try:
+            sys.stdout = sys.stderr
+            return old_input(*args)
+        finally:
+            sys.stdout = old_stdout
+
+    # Helper function to generate linker commands
+    # Converts library file names to linker equivalents
+    # e.g. 'libboost_graph.dylib' --> '-lboost_graph'
+    def generate_libs_string(lib_dir):
+        """Given a lib directory, generate a string containing linker commands"""
+        lib_string = ""
+        libs_set = set()
+        file_extensions = [".dylib", ".so"]
+        file_suffix = "lib"
+
+        for root, dirs, filenames in os.walk(lib_dir):
+            for fname in filenames:
+                extension = os.path.splitext(fname)[1]
+                if fname.startswith(file_suffix) and extension in file_extensions:
+                    curr_linker_command = fname.replace(extension, "") 
+                    curr_linker_command = curr_linker_command.replace(file_suffix, "-l")
+                    libs_set.add(curr_linker_command)
+
+            for libname in libs_set:
+                lib_string += " " + libname
+        
+        return lib_string
+    pkg_object = collections.OrderedDict() 
+    pkg_object['#'] = " Package Information for pkg-config"
+    if args.interactive:
+        print('To skip any of these prompts, simply press enter', file=sys.stderr)
+   
+
+    # Use input from user or command line options
+    prefix = (args.pkg_prefix == "" and args.interactive) and input('Prefix (e.g. /usr/local/Cellar/boost/1.60.0_2): ')  or args.pkg_prefix
+    pkg_name = (args.pkg_name == "" and args.interactive) and input('Package name (e.g. Boost): ') or args.pkg_name
+    description = (args.pkg_description == "" and args.interactive) and input("Package description: " ) or args.pkg_description
+    version = (args.pkg_version == "" and args.interactive) and input('Package Version (e.g. 1.60.0): ')  or args.pkg_version
+
+    pkg_object['prefix='] = prefix 
+    pkg_object['exec_prefix='] = "${prefix}"
+    pkg_object['libdir='] ="${exec_prefix}/lib"
+    pkg_object['includedir_old='] = "${prefix}/include/" + pkg_name.lower() 
+    pkg_object['includedir_new='] = "${prefix}/include"
+    pkg_object['Name: '] = pkg_name 
+    pkg_object['Description: '] = description 
+    pkg_object['Version: '] = version 
+    pkg_object['Libs: '] = generate_libs_string(args.lib_dir)
+    pkg_object['Cflags: '] = "-I${includedir_old} -I${includedir_new}" 
+
+    print('', file=sys.stderr)
+
+    f = None
+
+    # Print to stdout or to an output file (extension has to be .pc)
+    if args.output_file is not "":
+        path = args.output_file
+        while not path.endswith('.pc'):
+            print('The filename must contain the ".pc" extension. Please change your output file name accordingly')
+            path = input('File name (must end with .pc): ')
+        f = open(path, 'w') 
+    else:
+        f = sys.stdout
+    for key, value in pkg_object.items():
+        if key == 'prefix=' or key == 'Name: ':
+            print('', file=f)
+        print(key + value, file=f)
+
+def main():
+    args = create_parser().parse_args()
+    generate_package_file(args)
+
+if __name__ == "__main__":
+    main()
